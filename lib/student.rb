01ef87a51d819d49ab9ce970605fdce352a41315
Will Hodgson <will.hodgson@current-rms.com>
2019-10-02 09:13:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,6 +13,10 @@ class Student
   def self.create_table
     DB[:conn].execute("CREATE TABLE IF NOT EXISTS students(id INTEGER PRIMARY KEY, name TEXT, grade INTEGER)")
   end
+
+  def self.drop_table
+
+  end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
