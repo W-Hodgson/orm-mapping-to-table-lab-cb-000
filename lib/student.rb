618b3c91be87aec61f6404368cf0568262bc30f8
Will Hodgson <will.hodgson@current-rms.com>
2019-10-02 09:10:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,8 @@
 class Student
 
   def initialize(name, grade, id="nil")
-
+    @name = name
+    @grade = grade
   end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
