971a3e48752d2d8713b08d1abb95facad3e3daa5
Will Hodgson <will.hodgson@current-rms.com>
2019-10-02 09:19:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,7 @@ class Student
 
   def save
     DB[:conn].execute("INSERT INTO students (name, grade) VALUES (?, ?)", @name, @grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students ")
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students ")[0][0]
   end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
